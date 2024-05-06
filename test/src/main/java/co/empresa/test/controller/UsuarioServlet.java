74d6ea584e0302d722b76c13e7ee2e709613ebff
DavidRincon12 <115837263+DavidRincon12@users.noreply.github.com>
2024-05-06 10:49:25 -0500

    Add files via upload

--- test/src/main/java/co/empresa/test/controller/UsuarioServlet.java
+++ test/src/main/java/co/empresa/test/controller/UsuarioServlet.java
@@ -21,16 +21,16 @@ import co.empresa.test.modelo.Usuario;
 @WebServlet("/")
 public class UsuarioServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private UsuarioDao usuarioDao;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UsuarioServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UsuarioServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see Servlet#init(ServletConfig)
@@ -38,28 +38,28 @@ public class UsuarioServlet extends HttpServlet {
 	public void init(ServletConfig config) throws ServletException {
 		this.usuarioDao = new UsuarioDao();
 	}
-	
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String action = request.getServletPath();
-		
 		try {
-		switch(action) {
+		switch (action) {
 		case "/new":
-			showNewForm(request,response);
+			showNewForm(request, response);
 			break;
 		case "/insert":
-			insertarUsuario(request, response);
+			insertUsuario(request, response);
 			break;
 		case "/delete":
 			eliminarUsuario(request, response);
 			break;
 		case "/edit":
-			showEditForm(request,response);
+			showEditForm(request, response);
 			break;
 		case "/update":
 			actualizarUsuario(request, response);
@@ -67,92 +67,92 @@ public class UsuarioServlet extends HttpServlet {
 		default:
 			listUsuarios(request, response);
 			break;
-		}
-		}catch(SQLException e) {
+		}}
+		catch (SQLException e) {
 			throw new ServletException(e);
 		}
+		
 	}
 
-	
+
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
-	
-	private void showNewForm(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException{
+
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("usuario.jsp");
-		dispatcher.forward(request,response);
+		dispatcher.forward(request, response);
 	}
-	
-	private void insertarUsuario(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, SQLException, IOException{
+
+	private void insertUsuario(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException,  SQLException, IOException {
+
 		String nombre = request.getParameter("nombre");
 		String email = request.getParameter("email");
 		String pais = request.getParameter("pais");
 
-		Usuario usuario = new Usuario (nombre, email, pais);
-		
+		Usuario usuario = new Usuario(nombre, email, pais);
+
 		usuarioDao.insert(usuario);
+
+		response.sendRedirect("list");
+	}
+
+	
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, ServletException, IOException{
+		int id = Integer.parseInt(request.getParameter("id"));
+		Usuario usuarioActual = usuarioDao.select(id);
+		request.setAttribute("usuario", usuarioActual);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("usuario.jsp");
+		dispatcher.forward(request, response);
 		
+	}
+	
+	private void eliminarUsuario(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, SQLException, IOException {
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+
+
+		usuarioDao.delete(id);
+
 		response.sendRedirect("list");
 	}
 	
-	private void showEditForm(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException{
-		int id = Integer.parseInt(request.getParameter("id"));
+	private void actualizarUsuario(HttpServletRequest request, HttpServletResponse response)
+			throws  ServletException, SQLException, IOException {
 		
-		Usuario usuarioActual = usuarioDao.select(id);
-		
-		request.setAttribute("usuario", usuarioActual);
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("usuario.jsp");
-		dispatcher.forward(request,response);
-		
-	}
-		
-	private void actualizarUsuario(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, SQLException, IOException{
 		int id = Integer.parseInt(request.getParameter("id"));
 
 		String nombre = request.getParameter("nombre");
 		String email = request.getParameter("email");
 		String pais = request.getParameter("pais");
 
-		Usuario usuario = new Usuario (id, nombre, email, pais);
-		
+		Usuario usuario = new Usuario(id, nombre, email, pais);
+
 		usuarioDao.update(usuario);
-		
-		response.sendRedirect("list");
-	}
-		
-	
-	private void eliminarUsuario(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, SQLException, IOException{
-		int id = Integer.parseInt(request.getParameter("id"));
-		
-		usuarioDao.delete(id);
-		
+
 		response.sendRedirect("list");
 	}
 	
-	private void listUsuarios(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, SQLException, IOException{
-		System.out.println("Ajustes");
-		List <Usuario> listUsuarios = usuarioDao.selectAll();
+	private void listUsuarios(HttpServletRequest request, HttpServletResponse response)
+			throws  ServletException, SQLException, IOException {
+		List<Usuario> listUsuarios = usuarioDao.selectAll();
 		request.setAttribute("listUsuarios", listUsuarios);
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("usuariolist.jsp");
-		dispatcher.forward(request,response);
+		dispatcher.forward(request, response);
 		
-				
+		
 	}
 }
-	
-	
-
--- test/src/main/java/co/empresa/test/util/Conexion.java
+++ test/src/main/java/co/empresa/test/util/Conexion.java
@@ -7,61 +7,66 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Conexion {
+	
+	
+
 	private Connection con = null;
 	private static Conexion db;
 	private PreparedStatement preparedStatement;
-	
-	private static final String url= "jdbc:mysql://localhost:3306/sistema";
+
+	private static final String url = "jdbc:mysql://localhost:3306/";
 	private static final String dbName = "sistema";
-	private static final String driver = "com.mysql.cj.jdbc.Driver";
+	private static final String driver = "com.mysql.jdbc.Driver";
 	private static final String userName = "root";
 	private static final String password = "";
-	
-	public Conexion () {
+
+	public Conexion() {
 		try {
+
 			Class.forName(driver).newInstance();
-			con = (Connection)DriverManager.getConnection(url,userName,password);
-		} catch (InstantiationException | IllegalAccessException
-				| ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
+
+			con = (Connection) DriverManager.getConnection(url + dbName, userName, password);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-	}
-	public void cerrarConexion() {
-		try {
-			con.close();
-		} catch(SQLException e) {
-			e.printStackTrace();
-		}
+
 	}
 	
-	public static Conexion getConexion() {
-		if (db == null) {
-			db = new Conexion();
-		}
-		
-		return db;
-	}
 	
-	public ResultSet query() throws SQLException {
-		ResultSet res = preparedStatement.executeQuery();
+    public void cerrarConexion() {
+        try {
+            if (con != null && !con.isClosed()) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static Conexion getConexion() {
+    	if(db==null) {
+    		db = new Conexion();
+    	}
+    	return db;
+    }
+    
+    public ResultSet query() throws SQLException {
+    	ResultSet res = preparedStatement.executeQuery();
 		return res;
-	}
-	
-	public int execute() throws SQLException {
-		int result = preparedStatement.executeUpdate();
-		return result;
-	}
-	
-	public Connection getCon() {
-		return this.con;
-	}
-	
-	public PreparedStatement setPreparedStatement(String sql) throws SQLException {
-		this.preparedStatement = con.prepareStatement(sql);
-		return this.preparedStatement;
-		
-	}
+    }
+    
+    public int execute() throws SQLException{
+    	int result = preparedStatement.executeUpdate();
+    	return result;
+    }
+    
+    public Connection getCon() {
+    	return this.con;
+    }
+    
+    public PreparedStatement setPreparedStatement(String sql) throws SQLException {
+    	this.preparedStatement = con.prepareStatement(sql);
+    	return this.preparedStatement;
+    }
+    
 }
